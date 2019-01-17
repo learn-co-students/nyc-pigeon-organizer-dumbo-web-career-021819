b1031d7fbb51c6326a96bcb878aaa037d93ad619
jamesdorr3 <jamesdorr3@gmail.com>
2019-01-17 06:38:09 -0800

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  outputhash = {}
+  data.each do |category, infohash|
+    infohash.each do |detail, namearray|
+      namearray.each do |name|
+        if outputhash.key?(name)
+          if outputhash[name].key?(category)
+            outputhash[name][category] << detail.to_s
+          else
+            outputhash[name][category] = [detail.to_s]
+          end
+        else
+          outputhash[name] = {}
+          outputhash[name][category] = [detail.to_s]
+        end
+      end
+    end
+  end
+  outputhash
 end
