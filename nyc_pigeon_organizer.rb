e69cc4b813b241afc5952e52aa91dce940b4c5d0
adisagir <adisagir@gmail.com>
2019-02-07 23:46:27 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,37 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pidgeons = {}
+  data.each do |top_keys, all_details|
+    all_details.each do |key, value|
+      value.each do |names|
+        pidgeons[names] = { :color => [], :gender => [], :lives => [] }
+      end
+    end
+  end
+
+  data[:color].each do |key, value|
+    value.each do |names|
+      if data[:color][key].include?(names)
+        pidgeons[names][:color] << key.to_s
+      end
+    end
+  end
+
+  data[:gender].each do |key, value|
+    value.each do |names|
+      if data[:gender][key].include?(names)
+        pidgeons[names][:gender] << key.to_s
+      end
+    end
+  end
+
+  data[:lives].each do |key, value|
+    value.each do |names|
+      if data[:lives][key].include?(names)
+        pidgeons[names][:lives] << key
+      end
+    end
+  end
+  pidgeons
 end
