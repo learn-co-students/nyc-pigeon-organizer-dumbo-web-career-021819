2f468d154e2364ec913cbdef4925c64c48515d47
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-10 00:39:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_list = {}
+  pigeon_data.each do |attribute, details|
+    details.each do |detail, name_array|
+      name_array.each do |name|
+        pigeon_list[name] = {}
+      end
+    end
+  end
+  pigeon_data.each do |attribute, details|
+    details.each do |detail, name_array|
+      name_array.each do |name|
+        pigeon_list[name][attribute] = []
+      end
+    end
+  end
+  pigeon_data.each do |attribute, details|
+    details.each do |detail, name_array|
+      name_array.each do |name|
+        pigeon_list[name][attribute] << detail.to_s
+      end
+    end
+  end
+  pigeon_list
 end
+
