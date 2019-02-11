22627658fd59b9900ad73219d16aa1f4c7fef68e
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-02-11 21:40:29 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  
+  data.each { |info, specifics|
+    specifics.each {|specificsKey, namesArr|
+      namesArr.each {|name|
+        
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+          pigeon_list[name][info] = []
+        else 
+          pigeon_list[name][info] = []
+        end
+  
+      }
+    }
+  }
+  
+  pigeon_list.each {|names, categories|
+    categories.each {|catKey, arr|
+    data.each {|info, specifics|
+      specifics.each {|specificsKey, namesArr|
+        namesArr.each {|name|
+          if name == names && catKey == info
+            arr.push(specificsKey.to_s)
+          end
+        }
+      }
+    }
+    }
+  }
+  
+  binding.pry
+  return pigeon_list
 end
